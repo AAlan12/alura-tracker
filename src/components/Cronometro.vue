517d0cfd5bfa43e74e00b4ce1115d189cf55d5e4
Antonio Alan <tonio.alan07@gmail.com>
2022-10-13 09:44:00 -0300

    criação de interface, otimização componentes e de logica

--- src/components/Cronometro.vue
+++ src/components/Cronometro.vue
@@ -11,19 +11,19 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
     // eslint-disable-next-line vue/multi-word-component-names
-    name:'Cronometro',
-    props:{
-        tempoEmSegundos:{
+    name: 'Cronometro',
+    props: {
+        tempoEmSegundos: {
             type: Number,
             default: 0
         }
     },
-    computed:{
-        tempoDecorrido(): string{
-            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+    computed: {
+        tempoDecorrido(): string {
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8)
         }
     },
-    
+
 })
 
 </script>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -20,6 +20,7 @@ import Temporizador from './Temporizador.vue'
 export default defineComponent({
     // eslint-disable-next-line vue/multi-word-component-names
     name: "Formulário",
+    emits:['aoSalvarTarefa'],
     components: {
         Temporizador
     },
@@ -30,8 +31,10 @@ export default defineComponent({
     },
     methods: {
         finalizarTarefa(tempoDecorrido: number): void {
-            console.log('tempo decorrido: ', tempoDecorrido)
-            console.log('descrição da tarefa', this.descricao)
+            this.$emit('aoSalvarTarefa', {
+                duracaoEmSegundos: tempoDecorrido,
+                descricao: this.descricao
+            })
             this.descricao = ''
         }
     }
