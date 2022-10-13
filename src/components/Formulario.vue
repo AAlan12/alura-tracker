909020c029f13c6cf09b99588ca89f9a97389673
Antonio Alan <tonio.alan07@gmail.com>
2022-10-12 21:39:51 -0300

    criação do projeto e de componentes iniciais

---
+++ src/components/Formulario.vue
@@ -0,0 +1,62 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div class="box">
+        <div class="column">
+            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
+            </div>
+            
+            <div class="column">
+                <div class="is-flex is-align-items-center is-justify-content-space-between">
+                    <section>
+                        <strong>
+                            {{tempoDecorrido}}
+                        </strong>
+                    </section>
+                    <button class="button" @click="iniciar">
+                    <span class="icon">
+                        <i class="fas fa-play"></i>
+                    </span>
+                    <span>play</span>
+                    </button>
+                    <button class="button">
+                    <span class="icon" @click="finalizar">
+                        <i class="fas fa-stop"></i>
+                    </span>
+                    <span>stop</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    // eslint-disable-next-line vue/multi-word-component-names
+    name:'Formulário',
+    data(){
+        return{
+            tempoEmSegundos: 0,
+            cronometro: 0
+        }
+    },
+    computed:{
+        tempoDecorrido(): string{
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+        }
+    },
+    methods:{
+        iniciar(){
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos += 1
+            }, 1000)
+        },
+        finalizar(){
+            clearInterval(this.cronometro)
+        }
+    }
+});
+</script>
